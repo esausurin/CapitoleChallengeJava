c5f632338a2fcfda590d872e4eb6537f00567cbf
Eduardo Sausurin <falbrieu@costaisa.com>
2022-02-08 20:07:38 -0300

    en el adapter agrego la codigo para acceder al repository y mapeo de entity a BO

--- src/main/java/com/capitole/challenge/infrastructure/persistence/adapter/PriceAdapter.java
+++ src/main/java/com/capitole/challenge/infrastructure/persistence/adapter/PriceAdapter.java
@@ -6,19 +6,22 @@ import org.springframework.stereotype.Component;
 
 import com.capitole.challenge.domian.api.entity.Price;
 import com.capitole.challenge.domian.spi.PricePort;
+import com.capitole.challenge.infrastructure.persistence.mapper.PriceMapper;
 import com.capitole.challenge.infrastructure.persistence.repository.PriceRepository;
 
 @Component
 public class PriceAdapter implements PricePort {
 	private final PriceRepository repository;
-
-	public PriceAdapter(PriceRepository repository) {
+	private final PriceMapper mapper;
+	
+	public PriceAdapter(PriceRepository repository,
+			PriceMapper mapper) {
 		this.repository = repository;
+		this.mapper=mapper;
 	}
 	
 	@Override
 	public Price getPriceProductByDateProductBrandNativeQuery(LocalDateTime date, Integer product, Integer brand) {
-		// TODO Auto-generated method stub
-		return null;
+		return this.mapper.mapFromPersitence(this.repository.getPriceProductByDateProductBrand(date, product, brand));
 	}
 }
