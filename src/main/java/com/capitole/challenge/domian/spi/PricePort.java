f8f5c792027869e62c9088e305a8f15eafea0bb5
Eduardo Sausurin <falbrieu@costaisa.com>
2022-02-08 19:34:48 -0300

    creo el port y el adapter sin logica para acceder desde el dominio a la infraestructura

---
+++ src/main/java/com/capitole/challenge/domian/spi/PricePort.java
@@ -0,0 +1,8 @@
+package com.capitole.challenge.domian.spi;
+
+import java.time.LocalDateTime;
+
+import com.capitole.challenge.domian.api.entity.Price;
+public interface PricePort {
+	public Price getPriceProductByDateProductBrandNativeQuery(LocalDateTime date, Integer product, Integer brand);
+}
---
+++ src/main/java/com/capitole/challenge/infrastructure/persistence/adapter/PriceAdapter.java
@@ -0,0 +1,23 @@
+package com.capitole.challenge.infrastructure.persistence.adapter;
+
+import java.time.LocalDateTime;
+
+import org.springframework.stereotype.Component;
+
+import com.capitole.challenge.domian.api.entity.Price;
+import com.capitole.challenge.domian.spi.PricePort;
+
+@Component
+public class PriceAdapter implements PricePort {
+	private final PriceRepository repository;
+
+	public PriceAdapter(PriceRepository repository) {
+		this.repository = repository;
+	}
+	
+	@Override
+	public Price getPriceProductByDateProductBrandNativeQuery(LocalDateTime date, Integer product, Integer brand) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
